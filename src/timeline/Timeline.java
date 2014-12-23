470d0603aac9687b55611ec25788d01fab0f8422
AbrarMoemen <abrar.moemen@gmail.com>
2014-12-23 23:59:51 +0200

    Update Timeline.java

--- src/timeline/Timeline.java
+++ src/timeline/Timeline.java
@@ -1,12 +1,18 @@
 package timeline;
 
+import database.PostModel;
 import java.util.Vector;
+import post.Post;
+
+
 
 public abstract class Timeline {
 
     public Vector  hasA;
+     public PostModel myPostModel = new PostModel();
 
   public void getPosts() {
+      
   }
 
 }
