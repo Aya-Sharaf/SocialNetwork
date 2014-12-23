c6e0626e5e44a8503b6acfe17bd45b2ef27b9be6
AbrarMoemen <abrar.moemen@gmail.com>
2014-12-24 00:01:29 +0200

    Update ProfileTimeline.java

--- src/timeline/ProfileTimeline.java
+++ src/timeline/ProfileTimeline.java
@@ -1,8 +1,18 @@
 package timeline;
 
+import database.PostModel;
+
 public class ProfileTimeline extends Timeline {
 
+     
+    /**
+     *
+     */
+    @Override
   public void getPosts() {
+       myPostModel.getPost();
   }
+  
+  
 
 }
